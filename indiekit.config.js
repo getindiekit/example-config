1b30c9693028f94d7819d2f773767a2cdda32319
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-25 14:35:13 +0100

    feat: update config example

--- indiekit.config.js
+++ indiekit.config.js
@@ -1,40 +1,26 @@
 const process = require("node:process");
-
-/**
- * Customise post types.
- * 
- * Publication presets provide default values, but these can be overridden. 
- * See: https://getindiekit.com/configuration/post-types
- * 
- * Use placeholder tokens to customise file paths and URLS.
- * See: https://getindiekit.com/configuration/post-types#path-and-url-tokens
- */ 
-const postTypes = [{
-  type: "note",
-  name: "Note",
-  post: {
-    path: "_notes/{yyyy}-{MM}-{dd}-{slug}.md",
-    url: "notes/{yyyy}/{MM}/{dd}/{slug}",
-  },
-}];
+require("dotenv").config();
 
 module.exports = {
   /**
-   * Set application options
-   * 
+   * Application options
+   *
    * These values are used to configure application behaviour.
+   *
    * See: https://getindiekit.com/configuration/#application
-   */ 
+   */
   application: {
     mongodbUrl: process.env.MONGO_URL,
+    timeZone: process.env.TZ,
   },
+
   /**
-   * Add plug-ins.
-   * 
+   * Plug-ins
+   *
    * In this example we have chosen to use a publication preset for Jekyll,
-   * save posts and media files to GitHub repository, and added a syndicator
+   * save posts and media files to a GitHub repository, and added a syndicator
    * to share posts on a Mastodon server.
-   * 
+   *
    * See: https://getindiekit.com/configuration/#plugins
    */
   plugins: [
@@ -42,39 +28,42 @@ module.exports = {
     "@indiekit/store-github",
     "@indiekit/syndicator-mastodon",
   ],
+
   /**
-   * Set publication options
-   * 
+   * Publication options
+   *
    * These values are used to configure application behaviour.
+   *
    * See: https://getindiekit.com/configuration/#publication
-   */ 
+   */
   publication: {
-    me: "PUBLICATION_URL",
-    postTypes: postTypes,
+    me: process.env.PUBLICATION_URL,
   },
+
   /**
-   * GitHub content store options.
-   * 
+   * GitHub content store options
+   *
    * Other content stores are available.
+   *
    * See: https://getindiekit.com/plugins/stores
    */
   "@indiekit/store-github": {
-    user: "GITHUB_USERNAME",
-    repo: "GITHUB_REPOSITORY",
-    branch: "GITHUB_BRANCH",
-    token: process.env.GITHUB_TOKEN
+    user: process.env.GITHUB_USER,
+    repo: process.env.GITHUB_REPO,
+    branch: process.env.GITHUB_BRANCH,
   },
+
   /**
-   * Mastodon syndicator options.
+   * Mastodon syndicator options
    *
    * Multiple syndicators can be added.
+   *
    * See: https://getindiekit.com/plugins/syndicators
    */
   "@indiekit/syndicator-mastodon": {
     checked: true,
-    forced: true,
-    url: "MASTODON_SERVER",
-    user: "MASTODON_USERNAME",
-    accessToken: process.env.MASTODON_ACCESS_TOKEN
-  },
+    url: process.env.MASTODON_URL,
+    user: process.env.MASTODON_USER,
+    accessToken: process.env.MASTODON_ACCESS_TOKEN,
+  }
 };
